284a58310bb7ab195939e95b9982b712dde98648
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-20 17:25:02 +0900

    Dependency Installation

--- container/src/bootstrap.js
+++
@@ -1,8 +0,0 @@
-import { mount as productsMount } from "products/ProductsIndex";
-import { mount as cartMount } from "../../cart/src/bootstrap";
-import "cart/CartShow";
-
-console.log("container");
-
-productsMount(document.querySelector("#my-products"));
-cartMount(document.querySelector("#my-cart"));
