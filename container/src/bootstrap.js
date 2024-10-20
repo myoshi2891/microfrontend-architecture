82fb17f68d2085b150b6a36b55479f6ba0d3c39a
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-20 16:10:01 +0900

    Refactoring Cart

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,6 +1,8 @@
-import { mount } from "products/ProductsIndex";
+import { mount as productsMount } from "products/ProductsIndex";
+import { mount as cartMount } from "../../cart/src/bootstrap";
 import "cart/CartShow";
 
 console.log("container");
 
-mount(document.querySelector("#dev-products"));
+productsMount(document.querySelector("#my-products"));
+cartMount(document.querySelector("#my-cart"));
