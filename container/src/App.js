9b3a7903809dac70aa56e5058935b825017951ca
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-20 18:43:39 +0900

    Generic Integration

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { mount } from "marketing/MarketingApp";
-
-console.log(mount);
-
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-	return <h1>Hi There!</h1>;
+	return (
+		<>
+			<h1>Hi There</h1>
+			<hr />
+			<MarketingApp />
+		</>
+	);
 };
