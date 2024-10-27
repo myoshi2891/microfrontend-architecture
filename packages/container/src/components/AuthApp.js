d38ea059fc2bb5b3c143acf5c56839f8a8be3133
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-27 15:53:32 +0900

    Adding Initial State to Memory History

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -8,6 +8,7 @@ export default function AuthApp() {
 
 	useEffect(() => {
 		const { onParentNavigate } = mount(ref.current, {
+			initialPath: history.location.pathname,
 			onNavigate: ({ pathname: nextPathname }) => {
 				const { pathname } = history.location;
 
