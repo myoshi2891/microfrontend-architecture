58fb6f971273d4fb3dfb0e5c501224508a7e4396
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-27 16:28:33 +0900

    Lazily Loading SubApps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,10 @@
 import { createGenerateClassName, StylesProvider } from "@material-ui/styles";
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
-import MarketingApp from "./components/MarketingApp";
-import AuthApp from "./components/AuthApp";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
 	productionPrefix: "co",
@@ -15,10 +16,12 @@ export default () => {
 			<StylesProvider generateClassName={generateClassName}>
 				<div>
 					<Header />
-					<Switch>
-						<Route path="/auth" component={AuthApp} />
-						<Route path="/" component={MarketingApp} />
-					</Switch>
+					<Suspense fallback={<div>Loading...</div>}>
+						<Switch>
+							<Route path="/auth" component={AuthLazy} />
+							<Route path="/" component={MarketingLazy} />
+						</Switch>
+					</Suspense>
 				</div>
 			</StylesProvider>
 		</BrowserRouter>
